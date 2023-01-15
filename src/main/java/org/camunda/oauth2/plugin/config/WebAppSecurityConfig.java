8e9e0a37638b9c997f11caa5a96135f23134fd72
paul.lungu <paul.lungu@camunda.com>
2023-01-14 23:19:45 -0700

    inital commit

--- src/main/java/org/camunda/oauth2/plugin/config/WebAppSecurityConfig.java
+++ src/main/java/org/camunda/oauth2/plugin/config/WebAppSecurityConfig.java
@@ -6,13 +6,21 @@ import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.SecurityFilterChain;
 
 import java.util.Collections;
 
 @Order(SecurityProperties.BASIC_AUTH_ORDER - 15)
 public class WebAppSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final KeycloakLogoutHandler keycloakLogoutHandler;
+
+    WebAppSecurityConfig(KeycloakLogoutHandler keycloakLogoutHandler) {
+        this.keycloakLogoutHandler = keycloakLogoutHandler;
+    }
 
     @Override
     public void configure(WebSecurity web) throws Exception {
@@ -30,5 +38,20 @@ public class WebAppSecurityConfig extends WebSecurityConfigurerAdapter {
         filterRegistration.setOrder(101);
         filterRegistration.addUrlPatterns("/app/**");
         return filterRegistration;
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/camunda*")
+                .hasRole("USER")
+                .anyRequest()
+                .permitAll();
+        http.oauth2Login()
+                .and()
+                .logout()
+                .addLogoutHandler(keycloakLogoutHandler)
+                .logoutSuccessUrl("/");
+        return http.build();
     }
 }
